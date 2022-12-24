675f720f0d430330cc79974868900afd6c9b1eb5
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-12-24 13:08:54 -0500

    Adding 2-hypermedia_pagination.py file Implement a get_hyper method that takes the same arguments (and defaults) as get_page and returns a dictionary containing the following key-value pairs

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+Write a function named index_range that takes
+two integer arguments page and page_size
+"""
+import csv
+import math
+from typing import Tuple, List, Dict
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """
+    The function should return a tuple of size two containing a
+    start index and an end index corresponding to the range of
+    indexes to return in a list for those particular
+    pagination parameters
+    """
+    start_index = page_size * (page - 1)
+    end_index = start_index + page_size
+    result = (start_index, end_index)
+
+    return result
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Implement a method named get_page that takes two integer arguments
+        page with default value 1 and page_size with default value 10
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+
+        result = index_range(page, page_size)
+        start = int(result[0])
+        end = int(result[1])
+
+        return(self.dataset()[start: end])
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+        Implement a get_hyper method that takes the same arguments
+        (and defaults) as get_page and returns a dictionary containing
+        the following key-value pairs
+        """
+        result = self.get_page(page, page_size)
+        total_page = math.ceil(len(self.get_page(page_size=19419)) / page_size)
+        page_size = len(result)
+        page = page
+        data = result
+
+        if page < total_page:
+            next_page = page + 1
+        else:
+            next_page = None
+
+        if page > 1:
+            prev_page = page - 1
+        else:
+            prev_page = None
+
+        dict_result = {"page_size": page_size, "page": page, "data": data,
+                       "next_page": next_page, "prev_page": prev_page,
+                       "total_pages": total_page}
+
+        return dict_result
