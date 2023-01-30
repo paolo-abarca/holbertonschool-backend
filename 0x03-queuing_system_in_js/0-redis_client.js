183699ba508313f6493992decd07d63bc05d8aff
paolo-abarca <pao.lojag_1999@hotmail.com>
2023-01-30 10:51:01 -0500

    Updating 0-redis_client.js file write a script named 0-redis_client.js. It should connect to the Redis server running on your machine

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,9 +3,9 @@ import { createClient } from 'redis';
 const client = createClient();
 
 client.on('error', err => {
-	console.log(`Redis client not connected to the server: ${err.message}`, err)
+  console.log(`Redis client not connected to the server: ${err.message}`, err);
 });
 
 client.on('connect', () => {
-        console.log("Redis client connected to the server")
+  console.log("Redis client connected to the server");
 });
